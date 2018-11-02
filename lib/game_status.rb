790df03cfea7e394f770f91b1c56bac05c80721e
Stef Claus <stef.claus@gmail.com>
2018-11-02 12:46:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -77,4 +77,10 @@ def winner(board)
   end #end if statement 
 end #end win_combinations
 
-  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
